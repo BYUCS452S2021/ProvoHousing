a9ec79904724dabc91c18b025932c528a50d402c
Megan Johnson <mamberly@gmail.com>
2021-06-03 09:10:34 -0600

    add login and register lambda data

--- dynamo_lambda_src/login/main.py
+++ dynamo_lambda_src/login/main.py
@@ -1,9 +1,33 @@
 import boto3
 
-TABLE_NAME = 'foo'
+TABLE_NAME = 'Users'
+
 
 def lambda_handler(event, context):
     client = boto3.client('dynamodb')
+    user_id = event['userId']
+    password = event['password']
+    response = client.query(
+        ExpressionAttributeValues={
+            ':u': {
+                'S': user_id,
+            },
+            ":h": {
+                'N': "-1"
+            }
+        },
+        KeyConditionExpression='userId = :u AND roomId = :h',
+        ProjectionExpression='password',
+        TableName=TABLE_NAME,
+    )
+    resp = response['Items']
+    success = False
+    if len(resp) == 1:
+        pswd = resp[0]['password']['S']
+        success = pswd == password
+    else:
+        print("We had a problem finding the user")
     return {
-        "hello": "world"
+        "success": success,
+        "authToken": "token" if success else None
     }
--- dynamo_lambda_src/register/main.py
+++ dynamo_lambda_src/register/main.py
@@ -1,9 +1,39 @@
 import boto3
 
-TABLE_NAME = 'foo'
+TABLE_NAME = 'Users'
+
+
+def add_user(event):
+    client = boto3.client('dynamodb')
+    item_data = {
+        'roomId': {
+            'N': '-1',
+        },
+    }
+    for k, v in event.items():
+        if type(k) != str or type(v) != str:
+            print(f"Not including {k} because it is not a string")
+            continue
+        item_data[k] = {
+            'S': v
+        }
+    print(item_data)
+    response = client.put_item(
+        Item=item_data,
+        ReturnConsumedCapacity='TOTAL',
+        TableName=TABLE_NAME,
+    )
+    return 'ConsumedCapacity' in response and 'CapacityUnits' in response['ConsumedCapacity'] and \
+           response['ConsumedCapacity']['CapacityUnits'] == 1
+
 
 def lambda_handler(event, context):
-    client = boto3.client('dynamodb')
+    success = False
+    if 'userId' not in event or 'password' not in event:
+        print("Need userId or password")
+    else:
+        success = add_user(event)
     return {
-        "hello": "world"
+        "success": success,
+        "authToken": "token" if success else None
     }
